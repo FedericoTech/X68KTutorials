42d273cf227e74f0a53ea655a614035ca3f3393a
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2021-12-11 16:19:22 +0000

    some arrangements

--- FileOperations/main.c
+++ FileOperations/main.c
@@ -1,21 +1,27 @@
 #include <dos.h>
 #include <stdint.h>
 
+/**
+ * In this example we are creating a file, then storing a message in it and finally closing the file.
+ * Afterwards we open de file again in read mode and retrieve the message.
+ * At the end we remove the file.
+ */
+
 //permissions for using with _dos_open
-#define ACCESS_DICTIONARY 1 //not for users
-#define ACCESS_NORMAL 0
+#define ACCESS_DICTIONARY           0b10000000 //1 not for users
+#define ACCESS_NORMAL               0b00000000 //0
 
-#define SHARING_TOTAL 4                 //allow others to write and read
-#define SHARING_WRITE_ONLY 3            //allow others to write only
-#define SHARING_READ_ONLY 2             //allow others to read only
-#define SHARING_RESTRICTED 1            //don't allow others anything
-#define SHARING_COMPATIBILITY_MODE 0
+#define SHARING_TOTAL               0b00010000  //4 allow others to write and read
+#define SHARING_WRITE_ONLY          0b00001100  //3 allow others to write only
+#define SHARING_READ_ONLY           0b00001000  //2 allow others to read only
+#define SHARING_RESTRICTED          0b00000100  //1 don't allow others anything
+#define SHARING_COMPATIBILITY_MODE  0b00000000  //0
 
-#define MODE_RW 2                       //open for write and read
-#define MODE_W 1                        //open only for writing
-#define MODE_R 0                        //open only for reading
+#define MODE_RW                     0b00000010  //2 open for write and read
+#define MODE_W                      0b00000001  //1 open only for writing
+#define MODE_R                      0b00000000  //0 open only for reading
 
-#define OPENING_MODE(access, sharing, mode) ((access << 7) | (sharing << 2) | mode)
+#define OPENING_MODE(access, sharing, mode) (access | sharing | mode)
 
 int main(void)
 {
--- LoadPaletteDMA/utils.h
+++ LoadPaletteDMA/utils.h
@@ -1,19 +1,19 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
-#define ACCESS_DICTIONARY 1 //not for users
-#define ACCESS_NORMAL 0
+#define ACCESS_DICTIONARY           0b10000000 //1 not for users
+#define ACCESS_NORMAL               0b00000000 //0
 
-#define SHARING_TOTAL 4
-#define SHARING_WRITE_ONLY 3
-#define SHARING_READ_ONLY 2
-#define SHARING_RESTRICTED 1
-#define SHARING_COMPATIBILITY_MODE 0
+#define SHARING_TOTAL               0b00010000  //4 allow others to write and read
+#define SHARING_WRITE_ONLY          0b00001100  //3 allow others to write only
+#define SHARING_READ_ONLY           0b00001000  //2 allow others to read only
+#define SHARING_RESTRICTED          0b00000100  //1 don't allow others anything
+#define SHARING_COMPATIBILITY_MODE  0b00000000  //0
 
-#define MODE_RW 2
-#define MODE_W 1
-#define MODE_R 0
+#define MODE_RW                     0b00000010  //2 open for write and read
+#define MODE_W                      0b00000001  //1 open only for writing
+#define MODE_R                      0b00000000  //0 open only for reading
 
-#define OPENING_MODE(access, sharing, mode) ((access << 7) | (sharing << 2) | mode)
+#define OPENING_MODE(access, sharing, mode) (access | sharing | mode)
 
 #endif
