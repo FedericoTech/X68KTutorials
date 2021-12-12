2f269ec72ba95bc3d0bb3dbfdd90f49a76b17b8b
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2021-12-12 19:04:56 +0000

    arranges in FileOperations

--- FileOperations/main.c
+++ FileOperations/main.c
@@ -1,5 +1,7 @@
 #include <dos.h>
+#include <string.h>
 #include <stdint.h>
+#include <stdio.h>
 
 /**
  * In this example we are creating a file, then storing a message in it and finally closing the file.
@@ -23,8 +25,11 @@
 
 #define OPENING_MODE(access, sharing, mode) (access | sharing | mode)
 
+const char *getErrorMessage(int8_t code);
+
 int main(void)
 {
+
     int16_t file_number, status = 0;
 
     //We create a file and open it so we get its number to manage it.
@@ -36,6 +41,7 @@ int main(void)
     //if any error...
     if(file_number < 0){
         _dos_c_print("Can't create the file\r\n");
+        _dos_c_print(getErrorMessage(file_number));
         _dos_exit2(file_number);
     }
 
@@ -52,6 +58,7 @@ int main(void)
     //if any error...
     if(status < 0){
         _dos_c_print("Can't write in the file\r\n");
+        _dos_c_print(getErrorMessage(status));
         _dos_exit2(status);
     }
 
@@ -61,6 +68,7 @@ int main(void)
     //if any error...
     if(status < 0){
         _dos_c_print("Can't close the file\r\n");
+        _dos_c_print(getErrorMessage(status));
         _dos_exit2(status);
     }
 
@@ -77,6 +85,7 @@ int main(void)
     //if any error...
     if(file_number < 0){
         _dos_c_print("Can't open the file\r\n");
+        _dos_c_print(getErrorMessage(file_number));
         _dos_exit2(file_number);
     }
 
@@ -93,6 +102,7 @@ int main(void)
     //if any error...
     if(bytes_read < 0){
         _dos_c_print("Can't read from the file\r\n");
+        _dos_c_print(getErrorMessage(file_number));
         _dos_exit2(file_number);
     }
 
@@ -105,6 +115,7 @@ int main(void)
     //if any error...
     if(status < 0){
         _dos_c_print("Can't close the file\r\n");
+        _dos_c_print(getErrorMessage(status));
         _dos_exit2(status);
     }
 
@@ -114,10 +125,57 @@ int main(void)
     //if any error...
     if(status < 0){
         _dos_c_print("Can't delete the file\r\n");
+        _dos_c_print(getErrorMessage(status));
         _dos_exit2(status);
     }
 
     _dos_c_print("\r\nDone\r\n");
 
     _dos_exit();
+}
+
+const char *getErrorMessage(int8_t code)
+{
+    char *message;
+
+    switch(code)
+    {
+        case -1: message = "Executed invalid function code\r\n"; break;
+        case -2: message = "Specified file not found\r\n"; break;
+        case -3: message = "Specified directory not found\r\n"; break;
+        case -4: message = "Too many open files\r\n"; break;
+        case -5: message = "Cannot access directory or volume label\r\n"; break;
+        case -6: message = "Specified handle is not open\r\n"; break;
+        case -7: message = "Memory manager region was destroyed\r\n"; break;
+        case -8: message = "Not enough memory to execute\r\n"; break;
+        case -9: message = "Invalid memory manager pointer specified\r\n"; break;
+        case -10: message = "Illegal environment specified\r\n"; break;
+        case -11: message = "Abnormal executable file format\r\n"; break;
+        case -12: message = "Abnormal open access mode\r\n"; break;
+        case -13: message = "Error in selecting a filename\r\n"; break;
+        case -14: message = "Called with invalid parameter\r\n"; break;
+        case -15: message = "Error in selecting a drive\r\n"; break;
+        case -16: message = "Cannot remove current directory\r\n"; break;
+        case -17: message = "Cannot ioctrl device\r\n"; break;
+        case -18: message = "No more files found"; break;
+        case -19: message = "Cannot write to specified file\r\n"; break;
+        case -20: message = "Specified directory already registered\r\n"; break;
+        case -21: message = "Cannot delete because file exists\r\n"; break;
+        case -22: message = "Cannot name because file exists\r\n"; break;
+        case -23: message = "Cannot create file because disk is full\r\n"; break;
+        case -24: message = "Cannot create file because directory is full\r\n"; break;
+        case -25: message = "Cannot seek to specified location\r\n"; break;
+        case -26: message = "Specified supervisor mode with supervisor status on\r\n"; break;
+        case -27: message = "Thread with same name exists\r\n"; break;
+        case -28: message = "Interprocess communication buffer is write-protected\r\n"; break;
+        case -29: message = "Cannot start any more background processes\r\n"; break;
+        case -32: message = "Not enough lock regions\r\n"; break;
+        case -33: message = "Locked; cannot access\r\n"; break;
+        case -34: message = "Handler for specified drive is opened\r\n"; break;
+        case -35: message = "Symbolic link nest exceeded 16 steps (lndrv)\r\n"; break;
+        case -80: message = "File exists\r\n"; break;
+        default: message = "No error\r\n"; break;
+    }
+
+    return message;
 }
