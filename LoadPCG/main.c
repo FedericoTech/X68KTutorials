80df13dd2206a7a4bac14a1ceab471735b277041
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2022-01-25 14:06:44 +0000

    arrange loadpcg

--- LoadPCG/main.c
+++ LoadPCG/main.c
@@ -59,33 +59,48 @@ int main(void)
         _dos_exit2(status);
     }
 
-    //we open the palette file
-    file_handler = _dos_open(
-        "ship.pcg",
-        OPENING_MODE(
-            ACCESS_NORMAL,
-            SHARING_COMPATIBILITY_MODE,
-            MODE_R
-        )
-    );
-
-    //if any error...
-    if(file_handler < 0){
-        _iocs_crtmod(last_mode);
-        _dos_c_print("Can't open the file\r\n");
-        _dos_exit2(file_handler);
-    }
-
+    /**
+     * We are loading the PCG of the ship
+     */
     {
-        //we will collect the palettes pallete by pallete in this array.
-        uint32_t ship_pcg[8 * 4];
-
-        //we read the PCG of the ship from the file
-        _dos_read(
-            file_handler,       //file habdler
-            (char*)&ship_pcg,   //pointer to the buffet
-            sizeof(ship_pcg)    //size
-        );
+        uint32_t ship_pcg[8 * 4] = {
+            //tile 0
+            0x00000000,
+            0x0000000d,
+            0x0000000d,
+            0x000000cb,
+            0x000000cb,
+            0x000000cb,
+            0x003000cd,
+            0x003000cd,
+            //tile 1
+            0x00300bcd,
+            0x02320bcd,
+            0x0232bbcd,
+            0x0232bbcd,
+            0x0232bbcd,
+            0x023200cd,
+            0x055500cd,
+            0x00000055,
+            //tile 2
+            0x00000000,
+            0xd0000000,
+            0xd0000000,
+            0xbc000000,
+            0xbc000000,
+            0xbc000000,
+            0xdc000300,
+            0xdc000300,
+            //tile 3
+            0xdcb00300,
+            0xdcb02300,
+            0xdcb02300,
+            0xdcbb2300,
+            0xdcbb2300,
+            0xdc002300,
+            0xdc005550,
+            0x55000000,
+        };
 
         //we load the ship as a 16 x 16 tile in the position 0
         status = _iocs_sp_defcg(
@@ -100,22 +115,11 @@ int main(void)
             _dos_c_print("Can't load the PCG, Illegal screen mode.\r\n");
             _dos_exit2(status);
         }
-
-        //we load a second instance of the ship in position 1
-        status = _iocs_sp_defcg(
-            1,          // postition in 16 x 16 tiles
-            1,          // 16 x 16 tile
-            &ship_pcg   // pointer to the data
-        );
     }
 
-    //if any problem loading the PCG...
-    if(status < 0){
-        _iocs_crtmod(last_mode); //we restore the video mode
-        _dos_c_print("Can't load the PCG of the second ship, Illegal screen mode.\r\n");
-        _dos_exit2(status);
-    }
-
+    /**
+     * We are loading the PCG of the bullet
+     */
     {
         //PCG of bullet hardcoded
         uint32_t bullet[] = {
@@ -132,7 +136,7 @@ int main(void)
 
         //we load the hardcoded bullet on top of the second instance of the ship
         status = _iocs_sp_defcg(
-            6,          // postition in 8 x 8 tiles
+            6,          // position in 8 x 8 tiles
             0,          // 8 x 8 tile
             &bullet     // pointer to the data
         );
@@ -157,16 +161,6 @@ int main(void)
             _dos_c_print("Can't load the PCG of the bullet, Illegal screen mode.\r\n");
             _dos_exit2(status);
         }
-    }
-
-    //now we close the file
-    status = _dos_close(file_handler);
-
-    //if any error...
-    if(status < 0){
-        _iocs_crtmod(last_mode); //we restore the video mode
-        _dos_c_print("Can't close the file\r\n");
-        _dos_exit2(status);
     }
 
     _dos_c_print("Look into the PCG window of your emulator and press a key.\r\n");
--- LoadPCG/utils.c
+++ LoadPCG/utils.c
@@ -2,69 +2,37 @@
 
 int32_t loadPalette()
 {
-    int16_t file_handler;
     int32_t status;
 
-    //we open the palette file
-    file_handler = _dos_open(
-        "palette.pal",
-        OPENING_MODE(
-            ACCESS_NORMAL,
-            SHARING_COMPATIBILITY_MODE,
-            MODE_R
-        )
-    );
+    uint16_t colours[16] = {0x0000, 0x5294, 0x0020, 0x003e, 0x0400, 0x07c0, 0x0420, 0x07fe, 0x8000, 0xf800, 0x8020, 0xf83e, 0x8400, 0xffc0, 0xad6a, 0xfffe};
 
-    //if any error...
-    if(file_handler < 0){
-        _dos_c_print("Can't open the palette file\r\n");
-        return file_handler;
-    }
+    uint8_t colour_in_palette;
 
-    {
-        //we will collect the palettes pallete by pallete in this array.
-        uint16_t colours[16];
+    //now we go through the current palette
+    for(colour_in_palette = 0; colour_in_palette < 16; colour_in_palette++){
+        //we set the color
+        status = _iocs_spalet( //returns 32 bit integer
+            SET_VBD_V(
+                VERTICAL_BLANKING_DETECTION,
+                colour_in_palette   //0 - 15 if higher it only takes from 0-15
+            ),
+            1,  //first sprite palette
+            colours[colour_in_palette]
+        );
 
-        uint8_t palette_num = 1; //the sprite pallete count starts with 1. 0 belongs to text.
-
-        //whereas there are palettes...
-        while(_dos_read(file_handler, (char*)&colours, sizeof(colours))) {
-
-            uint8_t colour_in_palette;
-            //now we go through the current pallete
-            for(colour_in_palette = 0; colour_in_palette < 16; colour_in_palette++){
-                //we set the color
-                status = _iocs_spalet( //returns 32bit
-                    SET_VBD_V(
-                        VERTICAL_BLANKING_DETECTION,
-                        colour_in_palette   //0 - 15 if higher it only takes from 0-15
-                    ),
-                    palette_num,            //1-15 or 0
-                    colours[colour_in_palette]
-                );
-
-                //if any issue...
-                if(status < 0){
-                    switch(status){
-                    case -1:
-                        _dos_c_print("Incorrect screen mode\r\n");
-                        break;
-                    case -2:
-                        _dos_c_print("attempting to set palette in block 0\r\n");
-                        break;
-                    }
-                    return status;
-                }
+        //if any issue...
+        if(status < 0){
+            switch(status){
+            case -1:
+                _dos_c_print("Incorrect screen mode\r\n");
+                break;
+            case -2:
+                _dos_c_print("attempting to set palette in block 0\r\n");
+                break;
             }
-            palette_num ++;
+            return status;
         }
     }
-    //now we close the file
-    status = _dos_close(file_handler);
 
-    //if any error...
-    if(status < 0){
-        _dos_c_print("Can't close the palette file\r\n");
-        return status;
-    }
+    return 0;
 }
