6029adf020076a2cf3bf8b4b80d980f5b4f87c79
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2022-03-23 22:23:11 +0000

    Sonic example

---
+++ Sonic/main.c
@@ -0,0 +1,350 @@
+#include "utils.h"
+#include <signal.h>
+
+#include <stdio.h>
+
+#define DMA_DIR_A_TO_B 0
+#define DMA_DIR_B_TO_A 1
+
+#define DMA_FIXED 0
+#define DMA_PLUS_PLUS 1
+#define DMA_MINUS_3 2
+
+#define DMA_MODE(direction, MAC, DAC) (direction << 7 | (MAC << 2) | DAC)
+
+#define DMA_STATUS_IDLE 0x00
+#define DMA_STATUS_IN_DMAMOVE_OP 0x8A
+#define DMA_STATUS_IN_DMAMOV_A_OP 0x8B
+#define DMA_STATUS_IN_DMAMOV_L_OP 0x8C
+
+#define PCG_START 0xEB8000 // Start of PCG memory
+#define S_PALETTE_START 0xe82220 //Start of Palette
+
+//sprite palette utils
+#define VERTICAL_BLANKING_DETECTION 0x80000000     //0b10000000000000000000000000000000
+#define VERTICAL_BLANKING_NO_DETECT 0
+
+int8_t last_mode; //in this var we will store the video that was before we ran the program
+
+uint8_t *s_still;
+
+uint8_t *s_look_u;
+
+uint8_t *s_look_d;
+
+uint8_t *s_run[4];
+
+uint8_t *s_spin[4];
+
+uint8_t *s_wait[2];
+
+uint8_t *s_stop[2];
+
+uint8_t *s_walk[6];
+
+volatile int frame = 0;
+volatile int draw = 0;
+
+void terminate()			/* Processing routine when pressed CTRL-C */
+{
+    _dos_s_mfree(s_still);
+    _dos_s_mfree(s_look_u);
+    _dos_s_mfree(s_look_d);
+
+    _dos_s_mfree(s_run[0]);
+    _dos_s_mfree(s_run[1]);
+    _dos_s_mfree(s_run[2]);
+    _dos_s_mfree(s_run[3]);
+
+    _dos_s_mfree(s_spin[0]);
+    _dos_s_mfree(s_spin[1]);
+    _dos_s_mfree(s_spin[2]);
+    _dos_s_mfree(s_spin[3]);
+
+    _dos_s_mfree(s_wait[0]);
+    _dos_s_mfree(s_wait[1]);
+
+    _dos_s_mfree(s_stop[0]);
+    _dos_s_mfree(s_stop[1]);
+
+    _dos_s_mfree(s_walk[0]);
+    _dos_s_mfree(s_walk[1]);
+    _dos_s_mfree(s_walk[2]);
+    _dos_s_mfree(s_walk[3]);
+    _dos_s_mfree(s_walk[4]);
+    _dos_s_mfree(s_walk[5]);
+
+    //we restore the video mode
+    _iocs_crtmod(last_mode);
+}
+
+char * loadData(char * buffer, const char * filename, int8_t config)
+{
+    int16_t file_handler;
+    int32_t status;
+    int32_t status2;
+
+    void interrupt vsync_disp();
+
+    //we open the palette file
+    file_handler = _dos_open(
+        filename,
+        config
+    );
+
+    //if any error...
+    if(file_handler < 0){
+        terminate();
+        _dos_c_print("Can't open the file\r\n");
+        _dos_c_print(getErrorMessage(file_handler));
+
+        _dos_exit2(file_handler);
+    }
+
+    status = _dos_seek(
+       file_handler,
+       0, //offset
+       2  //0 = beginning, 1 = on the spot, 2 = end
+    );
+
+    _dos_seek(
+       file_handler,
+       0, //offset
+       0  //0 = beginning, 1 = on the spot, 2 = end
+    );
+
+    if(buffer == NULL){
+        buffer = _dos_malloc(status);
+    }
+
+    status2 = _dos_read(file_handler, buffer, status);
+
+    //now we close the file
+    status = _dos_close(file_handler);
+
+    //if any error...
+    if(status < 0){
+        terminate();
+        _dos_c_print("Can't close the file\r\n");
+        _dos_c_print(getErrorMessage(status));
+        _dos_exit2(status);
+    }
+
+    return buffer;
+} //loadData
+
+void interrupt vsync_disp()
+{
+    draw = 1;
+
+    if(++frame > 5){
+        frame = 0;
+    }
+}
+
+
+
+int main(void)
+{
+    int32_t status;
+
+    union FileConf fconf;
+
+    volatile uint16_t *palette_addr = (uint16_t *)S_PALETTE_START;
+
+    volatile uint16_t *pcg_addr = (uint16_t *)PCG_START;
+
+    last_mode = _iocs_crtmod(-1); //we capture the current video mode
+
+    status = _iocs_crtmod(10); // 8 this mode is 512 x 512 256 colours
+
+    _iocs_g_clr_on();
+    _iocs_b_curoff();
+
+    //load palette
+    fconf.config = 0;
+    fconf.flags.access_dictionary = ACCESS_NORMAL;
+    fconf.flags.sharing = SHARING_COMPATIBILITY_MODE;
+    fconf.flags.mode = MODE_R;
+
+    s_still = loadData(NULL, "s_still.spt", fconf.config);
+
+    s_look_u = loadData(NULL, "s_look_u.spt", fconf.config);
+
+    s_look_d = loadData(NULL, "s_look_d.spt", fconf.config);
+
+    s_run[0] = loadData(NULL, "s_run_0.spt", fconf.config);
+    s_run[1] = loadData(NULL, "s_run_1.spt", fconf.config);
+    s_run[2] = loadData(NULL, "s_run_2.spt", fconf.config);
+    s_run[3] = loadData(NULL, "s_run_3.spt", fconf.config);
+
+    s_spin[0] = loadData(NULL, "s_spin_0.spt", fconf.config);
+    s_spin[1] = loadData(NULL, "s_spin_1.spt", fconf.config);
+    s_spin[2] = loadData(NULL, "s_spin_2.spt", fconf.config);
+    s_spin[3] = loadData(NULL, "s_spin_3.spt", fconf.config);
+
+    s_wait[0] = loadData(NULL, "s_wait_0.spt", fconf.config);
+    s_wait[1] = loadData(NULL, "s_wait_1.spt", fconf.config);
+
+    s_stop[0] = loadData(NULL, "s_stop_0.spt", fconf.config);
+    s_stop[1] = loadData(NULL, "s_stop_1.spt", fconf.config);
+
+    s_walk[0] = loadData(NULL, "s_walk_0.spt", fconf.config);
+    s_walk[1] = loadData(NULL, "s_walk_1.spt", fconf.config);
+    s_walk[2] = loadData(NULL, "s_walk_2.spt", fconf.config);
+    s_walk[3] = loadData(NULL, "s_walk_3.spt", fconf.config);
+    s_walk[4] = loadData(NULL, "s_walk_4.spt", fconf.config);
+    s_walk[5] = loadData(NULL, "s_walk_5.spt", fconf.config);
+
+    _iocs_sp_init();
+    _iocs_sp_on();
+
+    _dos_super(0);
+    loadData((char*)palette_addr, "sonic.pal", fconf.config);
+    _dos_super(0);
+
+    {
+        int x = 50;
+        int y = 50;
+        union {
+            struct {
+                int8_t vf:1;
+                int8_t hf:1;
+                int8_t :2;           //padding
+                int8_t palette:4;
+                int8_t pcg;
+            } flags;
+            uint16_t code;
+        } sp_register;
+
+        sp_register.flags.vf = 0;         // VF ON
+        sp_register.flags.hf = 0;         // HF OFF
+        sp_register.flags.palette = 1;    // palette number
+        sp_register.flags.pcg = 0;        // pcg number
+
+        status = _iocs_sp_regst(
+            sp_register.flags.pcg | VERTICAL_BLANKING_NO_DETECT,    //int_ spno sprite number (0-127) //int_ mode bit 31 0: Vertical blanking interval detection post-setting 1: Not detected
+            x,                                  //int_ x X coordinates (0-1023 16 displayed on the far left
+            y,                                  //int_ y Y " (" " Top ")
+            sp_register.code,                   //code pattern code
+            3                       //int_ prw priority
+        );
+
+        sp_register.flags.pcg = 1;        // pcg number
+        status = _iocs_sp_regst(
+            sp_register.flags.pcg | VERTICAL_BLANKING_NO_DETECT,    //int_ spno sprite number (0-127) //int_ mode bit 31 0: Vertical blanking interval detection post-setting 1: Not detected
+            x + 16,                                  //int_ x X coordinates (0-1023 16 displayed on the far left
+            y,                                  //int_ y Y " (" " Top ")
+            sp_register.code,                   //code pattern code
+            3                       //int_ prw priority
+        );
+
+        sp_register.flags.pcg = 2;        // pcg number
+        status = _iocs_sp_regst(
+            sp_register.flags.pcg | VERTICAL_BLANKING_DETECTION,    //int_ spno sprite number (0-127) //int_ mode bit 31 0: Vertical blanking interval detection post-setting 1: Not detected
+            x + 32,                                  //int_ x X coordinates (0-1023 16 displayed on the far left
+            y,                                  //int_ y Y " (" " Top ")
+            sp_register.code,                   //code pattern code
+            3                       //int_ prw priority
+        );
+
+        sp_register.flags.pcg = 3;        // pcg number
+        status = _iocs_sp_regst(
+            sp_register.flags.pcg | VERTICAL_BLANKING_NO_DETECT,    //int_ spno sprite number (0-127) //int_ mode bit 31 0: Vertical blanking interval detection post-setting 1: Not detected
+            x,                                  //int_ x X coordinates (0-1023 16 displayed on the far left
+            y + 16,                                  //int_ y Y " (" " Top ")
+            sp_register.code,                   //code pattern code
+            3                       //int_ prw priority
+        );
+
+        sp_register.flags.pcg = 4;        // pcg number
+        status = _iocs_sp_regst(
+            sp_register.flags.pcg | VERTICAL_BLANKING_NO_DETECT,    //int_ spno sprite number (0-127) //int_ mode bit 31 0: Vertical blanking interval detection post-setting 1: Not detected
+            x + 16,                                  //int_ x X coordinates (0-1023 16 displayed on the far left
+            y + 16,                                  //int_ y Y " (" " Top ")
+            sp_register.code,                   //code pattern code
+            3                       //int_ prw priority
+        );
+
+        sp_register.flags.pcg = 5;        // pcg number
+        status = _iocs_sp_regst(
+            sp_register.flags.pcg | VERTICAL_BLANKING_DETECTION,    //int_ spno sprite number (0-127) //int_ mode bit 31 0: Vertical blanking interval detection post-setting 1: Not detected
+            x + 32,                                  //int_ x X coordinates (0-1023 16 displayed on the far left
+            y + 16,                                  //int_ y Y " (" " Top ")
+            sp_register.code,                   //code pattern code
+            3                       //int_ prw priority
+        );
+
+        sp_register.flags.pcg = 6;        // pcg number
+        status = _iocs_sp_regst(
+            sp_register.flags.pcg | VERTICAL_BLANKING_NO_DETECT,    //int_ spno sprite number (0-127) //int_ mode bit 31 0: Vertical blanking interval detection post-setting 1: Not detected
+            x,                                  //int_ x X coordinates (0-1023 16 displayed on the far left
+            y + 32,                                  //int_ y Y " (" " Top ")
+            sp_register.code,                   //code pattern code
+            3                       //int_ prw priority
+        );
+
+        sp_register.flags.pcg = 7;        // pcg number
+        status = _iocs_sp_regst(
+            sp_register.flags.pcg | VERTICAL_BLANKING_NO_DETECT,    //int_ spno sprite number (0-127) //int_ mode bit 31 0: Vertical blanking interval detection post-setting 1: Not detected
+            x + 16,                                  //int_ x X coordinates (0-1023 16 displayed on the far left
+            y + 32,                                  //int_ y Y " (" " Top ")
+            sp_register.code,                   //code pattern code
+            3                       //int_ prw priority
+        );
+
+        sp_register.flags.pcg = 8;        // pcg number
+        status = _iocs_sp_regst(
+            sp_register.flags.pcg | VERTICAL_BLANKING_DETECTION,    //int_ spno sprite number (0-127) //int_ mode bit 31 0: Vertical blanking interval detection post-setting 1: Not detected
+            x + 32,                                  //int_ x X coordinates (0-1023 16 displayed on the far left
+            y + 32,                                  //int_ y Y " (" " Top ")
+            sp_register.code,                   //code pattern code
+            3                       //int_ prw priority
+        );
+    }
+
+    signal(SIGINT, terminate);	/* Processing routine settings when is pressed CTRL-C */
+
+    _iocs_vdispst(
+        &vsync_disp,
+        1,//0: vertical blanking interval 1: vertical display period
+        1
+    );
+
+    _dos_c_print("Press a key.\r\n");
+
+    {
+        int cont;
+        int cont2;
+        int cont3;
+
+        while(1){
+            while(draw == 0){
+                ;
+            }
+
+            if(_iocs_dmamode()== DMA_STATUS_IDLE){
+
+                _iocs_dmamove(
+                    s_walk[frame],           //buffer A, the source
+                    (uint16_t*)pcg_addr,    //buffer B, the destination
+                    DMA_MODE(
+                         DMA_DIR_A_TO_B,    //from A to B
+                         DMA_PLUS_PLUS,     //move the pointer forward as it reads
+                         DMA_PLUS_PLUS      //move the pointer forward as it writes
+                    ),
+                    1152                    //size of the memory block we are moving
+                );
+            }
+
+            draw = 1;
+
+            if(_dos_inpout(0xFF) != 0){
+                break;
+            }
+        }
+    }
+
+    terminate();
+    _dos_exit();
+}
---
+++ Sonic/utils.h
@@ -0,0 +1,52 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#ifdef __MARIKO_CC__
+    #include <doslib.h>
+    #include <iocslib.h>
+#else
+    #include <dos.h>
+    #include <iocs.h>
+    #define interrupt __attribute__ ((interrupt_handler))
+#endif
+
+#include <stdint.h>
+
+#define ACCESS_DICTIONARY           0x80    //0b10000000 //1 not for users
+#define ACCESS_NORMAL               0x00    //0b00000000 //0
+
+#define SHARING_TOTAL               0x08    //0b00010000  //4 allow others to write and read
+#define SHARING_WRITE_ONLY          0x0C    //0b00001100  //3 allow others to write only
+#define SHARING_READ_ONLY           0x08    //0b00001000  //2 allow others to read only
+#define SHARING_RESTRICTED          0x04    //0b00000100  //1 don't allow others anything
+#define SHARING_COMPATIBILITY_MODE  0x00    //0b00000000  //0
+
+#define MODE_RW                     0x02    //0b00000010  //2 open for write and read
+#define MODE_W                      0x01    //0b00000001  //1 open only for writing
+#define MODE_R                      0x00    //0b00000000  //0 open only for reading
+
+#define OPENING_MODE(access, sharing, mode) (access | sharing | mode)
+
+union FileConf {
+    struct {
+        int8_t access_dictionary:1;
+        int8_t :2;      //padding
+        int8_t sharing:3;
+        int8_t mode:2;
+    } flags;
+    int8_t config;
+};
+
+#define SEEK_MODE_BEGINNING  0
+#define SEEK_MODE_CURRENT 1
+#define SEEK_MODE_END 2
+
+const char *getErrorMessage(int8_t code);
+
+uint32_t start_timer();
+
+uint32_t start_stop();
+
+uint32_t millisecond();
+
+#endif
