44be982ffab1213e48a2d7237db2826216e59d6f
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2023-08-24 00:22:13 +0100

    Resident

--- Resident/main.c
+++ Resident/main.c
@@ -1,272 +1,256 @@
 #ifdef __MARIKO_CC__
 	#include <doslib.h>
 	#include <iocslib.h>
+	#include <interrupt.h>
 #else
 	#include <dos.h>
 	#include <iocs.h>
 	#define interrupt __attribute__ ((interrupt_handler))
 #endif
 
-/*==========================================================*\
-| Resident program skeleton								 	|
-| Matsuura Hiroaki(hal@kmc.kyoto-u.ac.jp)					|
-| This program is free and without warranty. 				|
-| You are responsible for executing, modifying, 			|
-| and distributing modified products of this program. 		|
-\*==========================================================*/
+#include <math.h>
 
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<string.h>
 #define 	TIMER_C 	0x45		/* vector number to hook  MFP Timer-C (Mouse/cursor/FDD control, etc.) */
-#define 	KEYWORDS	"MMV"		/* keyword */
+#define 	KEYWORDS	"MMV"
 
-/*-------------------------------------------------------------*\
-| 0x000 Start of memory management pointer		   				|
-| 0x010 the beginning of the process management pointer (=_PSP) |
-| 0x100 Beginning of program area (=_PSTA)	   					|
-\*-------------------------------------------------------------*/
-#define 	OFFSET_PROCESS	0x10    //16
-#define 	OFFSET_PROGRAM	0x100   //256   OFFSET_PROGRAM - OFFSET_PROCESS = 240
+struct resident {
+    struct {
+        struct _mep mep; //16   bytes
+        struct _psp psp; //240 bytes
+    } procc;    //256 bytes
+    char keyword[4];    //here the program starts
+    short command;
+    void * oldvector;
+};
 
-/* The following three variables are set in the startup routine (__main). */
-#ifndef __STDLIB_H		/* stdlib.h is also defined, thus preventing double definition*/
-extern int _PSP;		/* Start address of process management pointer. Like _dos_getpdb() */
-#endif
-extern int _PSTA;		/* Start address of program area */
-extern int _HEND;		/* Last address of heap area */
-
-/*=================================================================*\
-| Permanent Department												|
-|																   	|
-| Routine that performs actual processing when resident. 			|
-| It is easier to process such as address calculation by placing	|
-| the keyword that serves as a mark when the resident is released	|
-| and the command passing part to the resident part at the beginning|
-| of the resident part.		   										|
-\*=================================================================*/
-/* Declare variables and functions in asm() with extern */
 extern char keyword[4];
-extern int oldvector;
-extern int command;
+extern short command;
+extern void * oldvector;
 
-void process_start();
 
-/* This is the start address (=_PSTA) of the program area */
-asm( "	_keyword:	.ds.b	4		" );	/* keyword */
-asm( "	_oldvector: .dc.l	0		" );	/* where to record the original vector */
-asm( "	_command:	.dc.l	0		" );	/* Command recipient location (not used) */
+asm( "	_keyword:	.ds.b	4	    " );
+asm( "	_command:   .ds.w	1		" );
+asm( "	_oldvector: .dc.l	0		" );
 
-asm( "	_process_start: 			" );	/* Resident section execution start address */
-asm( "	movem.l d0-d7/a0-a6,-(sp)	" );	/* save register */
-asm( "	bsr 	_prcs				" );	/* Call resident section main routine */
-asm( "	movem.l (sp)+,d0-d7/a0-a6	" );	/* register recovery */
-asm( "	move.l	_oldvector,-(sp)	" );	/* Event turnaround */
-asm( "	rts 						" );
+int counter;
+int collisions = 0;
+volatile char mutex = 1;
+int tick = 0;
 
-/*======================================================================================*\
-| Resident section main routine															 |
-|																						 |
-| Program here what you want to process in the resident part.							 |
-| Time-consuming processing and memory-tampering processing are definitely not allowed.	 |
-\*======================================================================================*/
-void prcs()
+const int centerX = 500;
+const int centerY = 100;
+const float armLength = 50.0f;
+const float PI = M_PI;
+
+#define TEXT_PLANE 2
+
+inline void my_print(const char*);
+
+//the resident program is this interrupt
+void interrupt process_start() //it has to be an interrupt
 {
-	int poi;
-	int mx, my, cx, cy;
+    //we check whether the interrupt is still being processed
+    if(mutex){
+        mutex = 0; //we hold the mutex
+        //only do things every 200 cycles
+        if(++counter % 200 == 0){
+            char buffer[50];
 
-	_iocs_ms_curon();
-	//we get mouse position which is given in a 32bit int
-	poi = _iocs_ms_curgt();
-	//we get the X from the most significant half
-	mx = (poi >> 16) & 0xffff;
-	//we get the Y from the less significant half
-	my = poi & 0xffff;
+            switch(command){
+                case 0: //just a text counter in the top-left corner
+                    sprintf(buffer, "counter: %d\n", counter);
+                    my_print(buffer);
+                    break;
+                case 1: //we are drawing something
+                    {
+                        {
+                            struct _txfillptr txfillptr = {
+                                /* unsigned short vram_page */  TEXT_PLANE, //0, 1, 2, 4. Plane 2 is fixed. It won't move up with the scroll
+                                /* short x */                   centerX - 50,
+                                /* short y */                   centerY - 50,
+                                /* short x1 */                  100,
+                                /* short y1 */                  100,
+                                /* unsigned short fill_patn */  0x0000
+                            };
 
-	//we get the cursor position
-	poi = _iocs_b_locate(-1, -1);
-	//we get the X from the most significant half
-	cx = (poi >> 16) & 0xffff;
-	//we get the Y from the less significant half
-	cy = poi & 0xffff;
+                            _iocs_txfill(&txfillptr);
+                        }
 
-	//we translate cursor coordinades to mouse coordinades
-	cx *= 8;
-	cy *= 16;
+                        {
+                            float angle = (float) (tick++ * 6); // Angle of rotation in degrees (adjust as needed)
 
-	//the mouse now goes after the cursor
-	if( mx > cx )	mx--;
-	if( mx < cx )	mx++;
-	if( my > cy )	my--;
-	if( my < cy )	my++;
+                            // Convert the angle to radians
+                            float angleRadians = (angle * PI) / 180.0f;
 
-	//we update the mouse's position.
-	_iocs_ms_curst(mx, my);
+                            // Calculate the endpoint coordinates based on the angle
+                            int endX = (int) armLength * cos(angleRadians);
+                            int endY = (int) armLength * sin(angleRadians); // Subtract because Y-axis is inverted
+
+
+
+                            struct _tboxptr tboxptr = {
+                                /* unsigned short vram_page */  TEXT_PLANE, //0, 1, 2, 4. Plane 2 is fixed. It won't move up with the scroll
+                                /* short x */                   centerX - 50,
+                                /* short y */                   centerY - 50,
+                                /* short x1 */                  100,
+                                /* short y1 */                  100,
+                                /* unsigned short line_style */ (0xf0 << (tick % 8)) | (0xf0 >> (8 - (tick % 8))) // dashed
+                            };
+
+                            //we draw this arm
+                            struct _tlineptr tlptr = {
+                                /* unsigned short vram_page */  TEXT_PLANE, //0, 1, 2, 4. Plane 2 is fixed. It won't move up with the scroll
+                                /* short x */                   centerX,
+                                /* short y */                   centerY,
+                                /* short x1 (length) */         endX,
+                                /* short y1 (length) */         endY,
+                                /* unsigned short line_style */ 0xff // solid
+                            };
+
+                            //we draw the box
+                            _iocs_txbox(&tboxptr);
+
+                            //we  draw the arm
+                            _iocs_txline(&tlptr);
+                        }
+                    }
+                    break;
+            }
+        }
+        mutex = 1; //we release the mutex
+    }
 }
 
-/*=================================*\
-| Non-resident department from here |
-\*=================================*/
-/*==========*\
-| How to use |
-\*==========*/
-void usage(char *execname, int *addr)
+//this function is inline to have a copy of it where ever is used
+inline void my_print(const char *message)
 {
-	_dos_c_print("\nResident program example\r\n");
-	printf("%s {-,/}(option)\n" , execname);
-	_dos_c_print("option: s   :be resident\r\n");
-	_dos_c_print("        r   :cancel the residency\r\n");
-	_dos_c_print("        t   :Let's test the execution of a resident routine without making it resident\r\n");
-	_dos_c_print("        SHIFT :Press key to exit\r\n");
-	_dos_c_print("        h,? :give this help\r\n\n");
-	printf("the current %s is resident\n", execname);
-	if(addr == NULL){
-		_dos_c_print("plug\r\n\n");
-	} else {
-		_dos_c_print("vinegar\r\n");
-		printf("Resident address $%x, Vector number $%x (original vector $%x)\n\n", addr, TIMER_C, oldvector);
-	}
-	_dos_exit2(1);
+    //we get the current position of the cursor
+    int cursor_pos = _iocs_b_locate(-1, -1);
+
+    //we move the cursor to the top left corner
+    _iocs_b_locate(0, 0);
+    //we print the message
+    _dos_c_print(message);
+    //we re establish cursor's position
+    _iocs_b_locate((cursor_pos >> 16) & 0xffff, cursor_pos & 0xffff);
 }
 
-/*===============*\
-| Temporary staff |
-\*===============*/
-void main(int argc, char **argv)
+
+
+//entry point
+int main(int argc, char *argv[])
 {
-	int ssp;
-	int *addr;
-    struct _psp *pspadr;
-/*-------------------------------------------------------------------------------------------------*\
-| Check if it is already resident.														          	|
-| When the processing of this part is finished, the variables take the following values.	        |
-|																		  			        		|
-|       when already resident		        		                   when not yet resident   		|
-|               addr	    resident process memory management pointer            NULL 				|
-\*-------------------------------------------------------------------------------------------------*/
+    struct resident * _resident, * resident_aux;
 
-    pspadr = _dos_getpdb();
+    { //this block is to find out whether the program is resident already or not
+        unsigned int resident_start_addr;
+
+        _resident = (struct resident *)((unsigned int) _dos_getpdb() - sizeof(struct _mep));    //we gather information about this instance
+        resident_start_addr = (unsigned int) _resident;                                         //including _mep and _psp
 
 
-    ssp = _iocs_b_super(0);
+        //we search for the resident process as long as there is a prev_mp address
+        while(resident_start_addr != 0){
 
-printf("pspadr == _PSP ? [%d]\n", (int)pspadr == _PSP);
-printf("struct _psp size [%d]\n", sizeof(struct _psp));
-printf("_PSP: %d\n", _PSP);
-printf("_PSP + size: %d\n", _PSP + sizeof(struct _psp));
-printf("_PSP - OFFSET_PROCESS: %d\n", _PSP - OFFSET_PROCESS);
-printf("_PSP - OFFSET_PROCESS + OFFSET_PROGRAM: %d\n", _PSP - OFFSET_PROCESS + OFFSET_PROGRAM);
-printf("_PSP - OFFSET_PROCESS + OFFSET_PROGRAM: [%d]\n", _PSTA);
-printf("_HEND: [%d]\n", _HEND);
+            char keyword[4] = {0};
 
-//printf("_PSP - 16 == _PSTA ? [%d]\n", (_PSP - 16) == _PSTA);
+            // we read a long word with _iocs_b_lpeek so that we don't need to go Super
+             resident_start_addr = _iocs_b_lpeek((void *) resident_start_addr);
 
-//OFFSET_PROCESS = 16
-//OFFSET_PROGRAM = 256
+             resident_aux = (struct resident *) resident_start_addr;
 
+            //we copy three byte in our buffer. we use _iocs_b_memstr so that we don't need to go Super
+            _iocs_b_memstr(
+                resident_aux->keyword,
+                keyword,
+                3
+            );
 
+            //printf("[%s]\n", keyword);
 
-    //pspadr = (void*)_PSP;
-	addr = (int*)(_PSP - OFFSET_PROCESS);	/* before memory management pointer */
-	oldvector = 0;
+            //if we find the process
+            if(strncmp(KEYWORDS, keyword, 3) == 0){
 
-	while( addr != NULL ){		/* NULL then it's over */
-		/* move to previous memory management pointer */
-		addr = (int*)*addr;
-		/* Compare top of program with keywords */
-		if( strncmp(KEYWORDS, (char*)((int)addr + OFFSET_PROGRAM), 3) == 0 ){
-			/* Take out the "original vector" stored in the resident part */
-			oldvector = *(int*)((int)addr + OFFSET_PROGRAM + 4);
-			break;
-		}
-	}
-    _iocs_b_super(ssp);
-/*-----------------------------------------------------------------*\
-| Examine the arguments. It is unkind to suddenly stay permanently! |
-\*-----------------------------------------------------------------*/
-	//if there is no params...
-	if(argc == 1){
-        //we show the menu
-		usage(argv[0], addr);
-	}
+                oldvector = resident_aux->oldvector;
 
-	if(
-		(argv[1][0] != '/')
-		&& (argv[1][0] != '-')
-	){
-		usage(argv[0], addr);
-	}
+                break;
+            }
+        }
+    } //end of the block to find out whether the program is resident.
 
-	switch( argv[1][1] ){
-		case 'H':
-		case 'h':
-		case '?':	usage(argv[0], addr);
-/*--------*\
-| Resident |
-\*--------*/
-		case 's':
-		case 'S':
-		{
-			if( addr != NULL ){
-				printf("%s is already resident. Can't stay any longer\n", argv[0]);
-				_dos_exit2(1);
-			}
-	/* set of keywords */
-			strcpy(keyword, KEYWORDS);
-	/* hook the vector */
-			oldvector = _iocs_b_intvcs(TIMER_C, (int)process_start);
-			printf("Vector number $%x hook up(original vector $%x)\n", TIMER_C, oldvector);
-	/* Resident end. */
-			printf("%s is resident.\n", argv[0]);
-			_dos_keeppr(_HEND - _PSTA, 0);
-		}
-/*---------------------*\
-| Resident cancellation |
-\*---------------------*/
-		case 'r':
-		case 'R':
-		{
-			if( addr == NULL ){
-				printf("%s is not resident\n", argv[0]);
-				_dos_exit2(1);
-			}
-	/* return vector */
-			_iocs_b_intvcs(TIMER_C, oldvector);
-			printf("Vector number $%x to the original vector $%x back to\n", TIMER_C, oldvector);
-	/* cancel residency */
-			printf("%s unpersisted.\n", argv[0]);
-			_dos_mfree((void*)((int)addr + OFFSET_PROCESS));
-			_iocs_ms_curof();
-			_dos_exit();
-		}
-/*---------------------*\
-| Resident routine test |
-\*---------------------*/
-		case 't':
-		case 'T':
-		{
-			volatile char *sft = (char*)0x80e;
+    //if no arguments we print help
+    if(argc == 1){
+        _dos_c_print("Resident program demonstration.\r\n");
 
-			if(addr == NULL){
-                ssp = _iocs_b_super(0);
-				while((*sft & 1) == 0){
-                    prcs();
+        printf("The residen program %s set.\n", oldvector == 0 ? "is not" : "is");
+
+        _dos_c_print(
+             "/s to set.\r\n"
+             "/r to remove.\r\n"
+             "/c [0-1] to change command.\r\n"
+         );
+
+    } else {
+        //we are setting the the resident process
+        if(strncmp("/s", argv[1], 2) == 0
+           || strncmp("/S", argv[1], 2) == 0
+        ){
+            //if there is no oldvector...
+            if(oldvector == 0){
+                unsigned int resident_addr, end_addr;
+                resident_addr = (unsigned int) &_resident->keyword;            //the program starts right where the keyword is.
+                end_addr = (unsigned int) _resident->procc.mep.block_end;      //where is where the program ends
+
+                //we set the keyword
+                _iocs_b_memstr(
+                    KEYWORDS,
+                    &keyword,
+                    3
+                );
+
+                //we get the current vector address
+                oldvector = _dos_intvcs(TIMER_C, process_start);
+                command = 1;
+
+                //we make the program resident
+                _dos_keeppr(
+                    end_addr - resident_addr,   //memory from beginning of the program
+                    0                           //exit code.
+                );
+
+                _dos_c_print("this line doesn't get to run\r\n");
+            } else {
+                _dos_c_print("The program is resident already\r\n");
+            }
+        }
+
+        //we are removing the resident process
+        if(strncmp("/r", argv[1], 2) == 0
+           || strncmp("/R", argv[1], 2) == 0
+        ){
+            if(oldvector != 0){
+                _dos_intvcs(TIMER_C, oldvector);
+                _dos_mfree(&resident_aux->procc.psp); //psp_addr
+
+                _dos_c_print("resident program stopped\r\n");
+            } else {
+                _dos_c_print("The program is not resident yet\r\n");
+            }
+        }
+
+        if(argc > 2){
+            if(strncmp("/c", argv[1], 2) == 0
+               || strncmp("/C", argv[1], 2) == 0
+            ){
+                char *endptr;
+                short num = strtol(argv[2], &endptr, 5);
+                if (*endptr == '\0') {
+                    resident_aux->command = num;
+                } else {
+                    _dos_c_print("Input is not a valid number.\r\n");
                 }
-                _iocs_b_super(ssp);
-				_iocs_ms_curof();
-				_dos_exit();
-			} else {
-				printf("%s is already resident. It does not test the execution of resident routines.\n", argv[0]);
-				_dos_exit2(1);
-			}
-		}
-/*--------------*\
-| optional error |
-\*--------------*/
-		default:
-		{
-			printf("invalid option%s\n", argv[1]);
-			usage(argv[0], addr);
-		}
-	}
+            }
+        }
+    }
+    _dos_exit();
 }
