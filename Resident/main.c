87b79f16dad2c1a150b2ba7cc97b35ac9735dae1
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2023-08-26 23:36:13 +0100

    resident example

--- Resident/main.c
+++ Resident/main.c
@@ -9,8 +9,9 @@
 #endif
 
 #include <math.h>
+#include <stdarg.h>
 
-#define 	TIMER_C 	0x45		/* vector number to hook  MFP Timer-C (Mouse/cursor/FDD control, etc.) */
+#define 	TIMER_C 	0x45		/* 0x45 vector number to hook  MFP Timer-C (Mouse/cursor/FDD control, etc.) */
 #define 	KEYWORDS	"MMV"
 
 struct resident {
@@ -33,18 +34,23 @@ asm( "	_command:   .ds.w	1		" );
 asm( "	_oldvector: .dc.l	0		" );
 
 int counter;
-int collisions = 0;
+int currentime = 0;
 volatile char mutex = 1;
-int tick = 0;
 
 const int centerX = 500;
 const int centerY = 100;
-const float armLength = 50.0f;
 const float PI = M_PI;
+const float radians = (float)M_PI / 180.0f;
+
+const float hour_arm_length = 32.0f;
+const float minute_arm_length = 40.0f;
+const float second_arm_length = 50.0f;
 
 #define TEXT_PLANE 2
 
-inline void my_print(const char*);
+void my_print(const char *format, ...);
+
+inline void calculate_arms(const float tick, const float armLength, short * endX, short * endY);
 
 //the resident program is this interrupt
 void interrupt process_start() //it has to be an interrupt
@@ -53,65 +59,177 @@ void interrupt process_start() //it has to be an interrupt
     if(mutex){
         mutex = 0; //we hold the mutex
         //only do things every 200 cycles
-        if(++counter % 200 == 0){
-            char buffer[50];
+        if(++counter % 100 == 0){
 
             switch(command){
                 case 0: //just a text counter in the top-left corner
-                    sprintf(buffer, "counter: %d\n", counter);
-                    my_print(buffer);
-                    break;
-                case 1: //we are drawing something
                     {
-                        {
-                            struct _txfillptr txfillptr = {
-                                /* unsigned short vram_page */  TEXT_PLANE, //0, 1, 2, 4. Plane 2 is fixed. It won't move up with the scroll
-                                /* short x */                   centerX - 50,
-                                /* short y */                   centerY - 50,
-                                /* short x1 */                  100,
-                                /* short y1 */                  100,
-                                /* unsigned short fill_patn */  0x0000
-                            };
+                        int auxtime = _dos_gettim2();
+                        my_print("counter: %d\n", counter);
 
-                            _iocs_txfill(&txfillptr);
+
+
+                        //if time has changed...
+                        if(auxtime != currentime){
+                            currentime = auxtime;
+
+                            my_print(
+                                "Time: %02d:%02d:%02d\n",
+                                currentime >> 16,
+                                (currentime >> 8) & 0x3F,
+                                currentime & 0x3F
+                            );
                         }
+                    }
+                    break;
+                case 1: //we are drawing an analog clock.
+                    {
+                        int auxtime = _dos_gettim2();
 
-                        {
-                            float angle = (float) (tick++ * 6); // Angle of rotation in degrees (adjust as needed)
+                        //if time has changed...
+                        if(auxtime != currentime){
+                            currentime = auxtime;
 
-                            // Convert the angle to radians
-                            float angleRadians = (angle * PI) / 180.0f;
+                            //clear clock
+                            {
+                                struct _txfillptr txfillptr = {
+                                    /* unsigned short vram_page */  TEXT_PLANE, //0, 1, 2, 4. Plane 2 is fixed. It won't move up with the scroll
+                                    /* short x */                   centerX - 50,
+                                    /* short y */                   centerY - 50,
+                                    /* short x1 */                  100,
+                                    /* short y1 */                  100,
+                                    /* unsigned short fill_patn */  0x0000
+                                };
 
-                            // Calculate the endpoint coordinates based on the angle
-                            int endX = (int) armLength * cos(angleRadians);
-                            int endY = (int) armLength * sin(angleRadians); // Subtract because Y-axis is inverted
+                                _iocs_txfill(&txfillptr);
+                            }
+
+                            {
+                                struct _tboxptr tboxptr = {
+                                    /* unsigned short vram_page */  TEXT_PLANE, //0, 1, 2, 4. Plane 2 is fixed. It won't move up with the scroll
+                                    /* short x */                   centerX - 50,
+                                    /* short y */                   centerY - 50,
+                                    /* short x1 */                  100,
+                                    /* short y1 */                  100,
+                                    /* unsigned short line_style */ (0xf0 << (counter % 8)) | (0xf0 >> (8 - (counter % 8))) // dashed
+                                };
+
+                                //we draw the box
+                                _iocs_txbox(&tboxptr);
+                            }
 
 
+                            {
+                                struct _tlineptr tlptr = {
+                                    /* unsigned short vram_page */  TEXT_PLANE, //0, 1, 2, 4. Plane 2 is fixed. It won't move up with the scroll
+                                    /* short x */                   centerX,
+                                    /* short y */                   centerY,
+                                    /* short x1 (length) */         0,  //for initialization purposes
+                                    /* short y1 (length) */         0,  //for initialization purposes
+                                    /* unsigned short line_style */ 0xff // solid
+                                };
 
-                            struct _tboxptr tboxptr = {
-                                /* unsigned short vram_page */  TEXT_PLANE, //0, 1, 2, 4. Plane 2 is fixed. It won't move up with the scroll
-                                /* short x */                   centerX - 50,
-                                /* short y */                   centerY - 50,
-                                /* short x1 */                  100,
-                                /* short y1 */                  100,
-                                /* unsigned short line_style */ (0xf0 << (tick % 8)) | (0xf0 >> (8 - (tick % 8))) // dashed
-                            };
+                                struct _ylineptr ylineptr = {
+                                    /* unsigned short vram_page */  TEXT_PLANE,
+                                    /* short x */                   centerX,
+                                    /* short y */                   centerY,
+                                    /* short y1 */                  0,  //for initialization purposes,
+                                    /* unsigned short line_style */ 0xff // solid
+                                };
 
-                            //we draw this arm
-                            struct _tlineptr tlptr = {
-                                /* unsigned short vram_page */  TEXT_PLANE, //0, 1, 2, 4. Plane 2 is fixed. It won't move up with the scroll
-                                /* short x */                   centerX,
-                                /* short y */                   centerY,
-                                /* short x1 (length) */         endX,
-                                /* short y1 (length) */         endY,
-                                /* unsigned short line_style */ 0xff // solid
-                            };
+                                struct _xlineptr xlineptr = {
+                                    /* unsigned short vram_page */  TEXT_PLANE,
+                                    /* short x */                   centerX,
+                                    /* short y */                   centerY,
+                                    /* short x1 */                  0,  //for initialization purposes,
+                                    /* unsigned short line_style */ 0xff // solid
+                                };
 
-                            //we draw the box
-                            _iocs_txbox(&tboxptr);
+                                char hour = (currentime >> 16);
+                                char minute = ((currentime >> 8) & 0x3F);
+                                char second = (currentime & 0x3F);
 
-                            //we  draw the arm
-                            _iocs_txline(&tlptr);
+
+                                float aux_hour = ((float) hour) + (1.0f / (float)minute);
+/*
+                                my_print(
+                                    "Time: %02d:%02d:%02d  length %f, %aux_hour:%f \n",
+                                    currentime >> 16,
+                                    (currentime >> 8) & 0x3F,
+                                    currentime & 0x3F,
+                                    hour_arm_length,
+                                    aux_hour
+                                );
+*/
+                                //hour
+                                //if the hour is in a vertical position...
+                                if(aux_hour == 12.0f || aux_hour == 24.0f || aux_hour == 6.0f || aux_hour == 18.0f){
+                                    ylineptr.y1 = (int)(hour_arm_length * (aux_hour == 12.0f || aux_hour == 24.0f ? 1.0f : -1.0f));
+
+                                    //we  draw the second arm vertically
+                                    _iocs_txyline(&ylineptr);
+
+                                //if the hour is in an horizontal position...
+                                } else if (aux_hour == 9.0f || aux_hour == 21.0f || aux_hour == 3.0f || aux_hour == 15.0f){
+                                     xlineptr.x1 = (int)(hour_arm_length * (aux_hour == 9.0f || aux_hour == 21.0f ? -1.0f : 1.0f));
+
+                                     //we  draw the second arm horizontally
+                                    _iocs_txxline(&xlineptr);
+                                //if in another position...
+                                } else {
+                                    calculate_arms(
+                                       aux_hour * 5.0f,
+                                       hour_arm_length,
+                                       &tlptr.x1,
+                                       &tlptr.y1
+                                    );
+                                    //we  draw the second arm
+                                    _iocs_txline(&tlptr);
+                                }
+
+                                //minute
+                                //if the minute is in a vertical position...
+                                if(minute == 0 || minute == 30){
+                                    ylineptr.y1 = (int)second_arm_length * (minute == 30 ? 1 : -1);
+
+                                    //we  draw the minute arm vertically
+                                    _iocs_txyline(&ylineptr);
+                                //if the minute is in an horizontal position...
+                                } else if(minute == 15 || minute == 45) {
+                                    xlineptr.x1 = (int)second_arm_length * (minute == 45 ? -1 : 1);
+
+                                    //we  draw the second arm horizontally
+                                    _iocs_txxline(&xlineptr);
+                                //if in another position...
+                                } else {
+                                    calculate_arms(minute, minute_arm_length, &tlptr.x1, &tlptr.y1);
+
+                                    //we  draw the second arm
+                                    _iocs_txline(&tlptr);
+                                }
+
+                                //second
+                                //if the second is in a vertical position...
+                                if(second == 0 || second == 30){
+                                    ylineptr.y1 = (int)second_arm_length * (second == 30 ? 1 : -1);
+
+                                    //we  draw the second arm vertically
+                                    _iocs_txyline(&ylineptr);
+                                //if the second is in a vertical position...
+                                } else if(second == 15 || second == 45) {
+                                    xlineptr.x1 = (int)second_arm_length * (second == 45 ? -1 : 1);
+
+                                    //we  draw the second arm horizontally
+                                    _iocs_txxline(&xlineptr);
+                                //if in another position...
+                                } else {
+
+                                    calculate_arms(second, second_arm_length, &tlptr.x1, &tlptr.y1);
+
+                                    //we  draw the second arm
+                                    _iocs_txline(&tlptr);
+                                }
+                            }
                         }
                     }
                     break;
@@ -121,18 +239,36 @@ void interrupt process_start() //it has to be an interrupt
     }
 }
 
+inline void calculate_arms(const float tick, const float armLength, short * endX, short * endY)
+{
+    float angle = tick * 6.0f - 90.0f; // Angle of rotation in degrees (adjust as needed)
+
+    // Convert the angle to radians
+    float angleRadians = radians * angle;
+
+    // Calculate the endpoint coordinates based on the angle
+    *endX = (short) armLength * cos(angleRadians);
+    *endY = (short) armLength * sin(angleRadians); // Subtract because Y-axis is inverted
+}
+
+
 //this function is inline to have a copy of it where ever is used
-inline void my_print(const char *message)
+void my_print(const char *format, ...)
 {
     //we get the current position of the cursor
     int cursor_pos = _iocs_b_locate(-1, -1);
 
+    va_list args;
+    va_start(args, format);
+
     //we move the cursor to the top left corner
     _iocs_b_locate(0, 0);
     //we print the message
-    _dos_c_print(message);
+    //_dos_c_print(message);
+    vprintf(format, args);
     //we re establish cursor's position
     _iocs_b_locate((cursor_pos >> 16) & 0xffff, cursor_pos & 0xffff);
+    va_end(args);
 }
 
 
@@ -242,12 +378,18 @@ int main(int argc, char *argv[])
             if(strncmp("/c", argv[1], 2) == 0
                || strncmp("/C", argv[1], 2) == 0
             ){
-                char *endptr;
-                short num = strtol(argv[2], &endptr, 5);
-                if (*endptr == '\0') {
-                    resident_aux->command = num;
+                //if the program is resident...
+                if(oldvector != 0){
+                    char *endptr;
+                    short num = strtol(argv[2], &endptr, 5);
+                    if (*endptr == '\0') {
+                        resident_aux->command = num;
+                    } else {
+                        _dos_c_print("Input is not a valid number.\r\n");
+                    }
+                //if the program isn't resident yet...
                 } else {
-                    _dos_c_print("Input is not a valid number.\r\n");
+                    _dos_c_print("The program is not resident yet\r\n");
                 }
             }
         }
